fae6fdcc4ec406927b814d36e98ee042a645961d
redengin <sholstei@yahoo.com>
2023-06-29 03:37:18 -0400

    in work

--- lib/ebook.dart
+++ lib/ebook.dart
@@ -1,2 +1,35 @@
 library ebook;
 
+import 'dart:convert';
+
+import 'package:archive/archive_io.dart';
+import 'package:xml/xml.dart';
+
+class EBook {
+  late final Archive _archive;
+
+  EBook(String path) {
+    _archive = ZipDecoder().decodeBuffer(InputFileStream(path));
+  }
+
+  bool validateMimeType() {
+    var file = _archive.findFile("mimetype");
+    if(file == null) return false;
+    return String.fromCharCodes(file.content) == "application/epub+zip";
+  }
+
+  bool validateContainer() {
+    var file = _archive.findFile("META-INF/container.xml");
+    if(file == null) return false;
+    var containerDocument = XmlDocument.parse(utf8.decode(file.content));
+    var roots = containerDocument.findAllElements("rootfile");
+    return roots.length == 1;
+  }
+
+// final ZipFile zipfile;
+//
+// Ebook(istream) {
+//   this.zipFile = ZipFile(istream);
+// }
+//
+}
--- test/ebook_test.dart
+++ test/ebook_test.dart
@@ -1,12 +1,29 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 
 import 'package:ebook/ebook.dart';
+import 'dart:io';
+import 'package:path/path.dart' as Path;
 
-void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+final Uri referenceEbookUrl = Uri.parse(
+    "https://github.com/IDPF/epub3-samples/releases/download/20170606/accessible_epub_3.epub");
+
+void main() async {
+  final request = await HttpClient().getUrl(referenceEbookUrl);
+  final response = await request.close();
+  final tmpDir = await Directory(".").createTemp("ebook-test-");
+  final tmpFile = await File(Path.join(tmpDir.path, "ebook.pub"));
+  final ebook_data = await response.pipe(
+    tmpFile.openWrite()
+  );
+  final ebook = EBook(tmpFile.path);
+
+  test('validate epub3 mimetype', () {
+    assert(ebook.validateMimeType());
   });
+
+  test('validate META-INF/container.xml', () {
+    assert(ebook.validateContainer());
+  });
+
+  tmpDir.delete(recursive: true);
 }
